41f0fac877d7a0a14e0aa3da2498f042e3bbb605
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-25 01:27:25 +0900

    add slack env

---
+++ api/report-dev-error.ts
@@ -0,0 +1,32 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { ApiRequestBody } from "../types/RequestType";
+import { SlackWebhookPayload } from "../types/SlackType";
+
+export default async (req: NowRequest, res: NowResponse) => {
+  if (req.method !== "POST") {
+    res.status(403).json({ error: `${req.method} is invalid request method` });
+  }
+  const { body } = req;
+  if (!_isValidVercelRequest(body)) {
+    res.json({ error: "error" });
+    return;
+  }
+
+  const objectForSlack: SlackWebhookPayload = {
+    text: body.message,
+  };
+
+  await fetch(`${process.env.SLACK_WEBHOOK_URL_DEV}`, {
+    method: "POST",
+    body: JSON.stringify(objectForSlack),
+  });
+  res.status(204).send("");
+};
+
+const _isValidVercelRequest = (body: any): body is ApiRequestBody => {
+  if (typeof body.message !== "string") {
+    console.error("invalid id type.");
+    return false;
+  }
+  return true;
+};
